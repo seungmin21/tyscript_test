dde615d4a1ea7de333bddf7aa0ee0adf8383ec45
ohseungmin <ohmin2886@gmail.com>
2023-11-27 19:02:52 +0900

    이렇게도 할 수 있는지 테스트

--- app.ts
+++ app.ts
@@ -1,4 +1,17 @@
-// 직접 생성한 app.ts 파일
-function greet(name: string) {
-  return `Hello, ${name}!`;
+// app.ts 파일 수동 생성
+let age : number = 30; 
+let isType : boolean = true;
+let message = "hello, world";
+interface Person {
+  name: string;
+  age: number;
+}
+
+type Point = {
+  x : number;
+  y : number;
+}
+
+function addition(hello: string, world: string) : string {
+  return hello + world;
 }
